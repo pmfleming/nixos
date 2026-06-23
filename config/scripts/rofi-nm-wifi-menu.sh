9f673c8ef096cae3018fc658b68f31005c5e65bf
Paul Fleming <67100074+pmfleming@users.noreply.github.com>
2026-06-23 13:38:52 +0200

    Auto-refresh progressive Wi-Fi rescan

--- config/scripts/rofi-nm-wifi-menu.sh
+++ config/scripts/rofi-nm-wifi-menu.sh
@@ -4,4 +4,5 @@ set -eu
 exec rofi \
   -show nm-wifi \
   -modi "nm-wifi:nm-wifi-rofi rofi" \
-  -kb-custom-1 "Alt+r"
+  -kb-custom-1 "Alt+r" \
+  -theme-str 'configuration { timeout { delay: 1; action: "kb-custom-2"; } }'
